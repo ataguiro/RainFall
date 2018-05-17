a54e69fa99a1de19fec4616365351a229e567b8e
root <root@localhost.localdomain>
2018-05-17 18:50:20 +0000

    [level2] walkthrough + source (TOCHECK

---
+++ level2/source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <>
+
+char	*p(void)
+{
+	void	*tmp;
+	char	buffer[64];
+
+	fflush(stdout);
+	gets(buffer);
+
+	register void *p asm("ebp+0x4");
+	if ((p & 0xb0000000) - 0xb0000000) {
+		printf("(%p)\n", p);
+		exit(1);
+	}
+	puts(buffer);
+	return (strdup(buffer));
+}
+
+int	main(void)
+{
+	return (p());
+}
