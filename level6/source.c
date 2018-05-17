32a239ed68a75d8e4689e2ebc669c8117a9a3de0
root <root@localhost.localdomain>
2018-05-17 21:04:41 +0000

    [level6] walkthrough ok, source TO END

---
+++ level6/source.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int	n(void)
+{
+	system("/bin/cat /home/user/level7/.pass");
+	return (0);
+}
+
+int	m(void)
+{
+	puts("Nope");
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*p1;
+	long	*p2;
+
+	p1 = malloc(64);
+	p2 = malloc(8);
+
+	memcpy(p2, (long)m, 8);
+	printf("%llx\n", *p2);
+	printf("%p\n", m);
+
+/*	strcpy(p1, argv[1]); */
+/*	p2();	*/
+	return (0);
+}
