a4ee3b426b2a1f19446e6e64d19cb2fd1aa768b1
root <root@localhost.localdomain>
2018-05-17 20:20:24 +0000

    [level5] walkthrough + source

---
+++ level5/source.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void	o(void)
+{
+	system("/bin/sh");
+	exit(1);
+}
+
+
+void	n(void)
+{
+	char	buffer[520];
+
+	fgets(buffer, 512, stdin);
+	printf(buffer);
+	exit(1);
+}
+
+int	main(oid)
+{
+	return (n());
+}
