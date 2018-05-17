87563992f86e7f1c9fe0622556658669dbdbf37e
root <root@localhost.localdomain>
2018-05-17 16:56:11 +0000

    [level0] source.c ok

--- level0/source.c
+++ level0/source.c
@@ -1,9 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#define _GNU_SOURCE
 
 int	main(int argc, char **argv)
 {
 	char	*sh;
 	char	*tmp;
+	uid_t	uid;
 	gid_t	id;
+	char	*arg[2];
 	/* esp, 0x20 */
 
 	if (atoi(argv[1]) == 423) {
@@ -11,9 +18,16 @@ int	main(int argc, char **argv)
 		tmp = NULL;
 
 		id = getegid();
-		getuid()
-		setresgid;
-		setresuid;
+		uid = geteuid();
+
+		setresgid(id, id, id);
+		setresuid(uid, uid, uid);
 		
-		execv
+		arg[0] = sh;
+		arg[1] = NULL;
+		execv("/bin/sh", arg);
 	} else {
+		write(2, "No !\n", 5);
+	}
+	return (0);
+}
