8bfe1183cc6a9d150c790238a0016855b204b11d
root <root@localhost.localdomain>
2018-05-17 15:17:11 +0000

    [level0] walkthrough + source doing

---
+++ level0/source.c
@@ -0,0 +1,19 @@
+
+int	main(int argc, char **argv)
+{
+	char	*sh;
+	char	*tmp;
+	gid_t	id;
+	/* esp, 0x20 */
+
+	if (atoi(argv[1]) == 423) {
+		sh = strdup("/bin/sh");
+		tmp = NULL;
+
+		id = getegid();
+		getuid()
+		setresgid;
+		setresuid;
+		
+		execv
+	} else {
