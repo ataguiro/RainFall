42a590c1d1ade2d6d5aaf3eaf2afdd7e40b19daa
root <root@localhost.localdomain>
2018-05-17 21:41:08 +0000

    [level7] source + walkthrough ok

---
+++ level7/source.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char	ptr[68];
+
+int	m(void)
+{
+	printf("%s - %d\n", "", time(0));
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*tmp;
+	char	*tmp2;
+
+	tmp = malloc(8);
+	*tmp = 1;
+
+	*(tmp + 4) = malloc(8);
+
+	tmp2 = malloc(8);
+	*tmp2 = 2;
+	*(tmp2 + 4) = malloc(8);
+
+	strcpy(tmp + 4, argv[1]);
+	strcpy(tmp2 + 4, argv[1]);
+
+	/* fgets(ptr, 0x44, fopen("/home/user/level8/.pass", "r")); */
+	puts("~~");
+	return (0);
+}
